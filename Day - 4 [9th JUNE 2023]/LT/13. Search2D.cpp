d2798e15d822f334ceaeeaf5b336c2e722eba7e0
Bhashit Rohit <36413665+bhashit97@users.noreply.github.com>
2023-06-10 04:14:50 +0530

    Update 13. Search2D.cpp

--- Day - 4 [9th JUNE 2023]/LT/13. Search2D.cpp
+++ Day - 4 [9th JUNE 2023]/LT/13. Search2D.cpp
@@ -1,1 +1,35 @@
-PL
+// PL : https://leetcode.com/problems/search-a-2d-matrix/description/
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        // search in like virtual array
+        if(!matrix.size())
+        return false;
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int low = 0;
+        int high = (n*m)-1;
+
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            
+            if(matrix[mid/m][mid%m]==target)
+            return true;
+            else if(matrix[mid/m][mid%m]<target)
+            {
+                low = mid+1;
+            }
+            else
+            {
+                high = mid-1;
+            }
+        }
+        return false;
+        
+    }
+};
+Console
+
