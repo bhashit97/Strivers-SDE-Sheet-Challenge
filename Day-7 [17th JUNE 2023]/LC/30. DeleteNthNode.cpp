d489ec0bbb48f3ba818db37289cffcbac94d06f2
Bhashit Rohit <36413665+bhashit97@users.noreply.github.com>
2023-06-18 03:53:57 +0530

    Create 30. DeleteNthNode.cpp

---
+++ Day-7 [17th JUNE 2023]/LC/30. DeleteNthNode.cpp
@@ -0,0 +1,9 @@
+//PL : https://leetcode.com/problems/delete-node-in-a-linked-list/
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
